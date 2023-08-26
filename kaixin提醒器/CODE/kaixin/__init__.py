8c55a203fa94aeac71475ac8877e18ff6bd2a29d
kaixin168sxz <122666772+kaixin168sxz@users.noreply.github.com>
2023-08-26 10:09:29 +0800

    Add files via upload

---
+++ kaixin提醒器/CODE/kaixin/__init__.py
@@ -0,0 +1,11 @@
+# --$KaixinPackage>2.0.9$-- #
+# Produced by Kaixin(SongXinZhe, 宋昕哲) #
+# Time: 2023.7.12 #
+# Region: China, 中国 #
+# Editor: PyCharm>2023.1.0 #
+# Language: Python>3.8 #
+
+
+from .kaixin import *
+
+__all__ = ['GetTime', 'Translation', 'GetWeather', 'Files', 'Joystick', 'Speech', 'PlaySound', 'Record', 'check_process_running']
